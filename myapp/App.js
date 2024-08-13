348e6673656bbe78b32cf748eecd01671f6effb5
Adpro813 <adityad1108@gmail.com>
2024-08-12 17:37:26 -0700

    change background color to cream

--- myapp/App.js
+++ myapp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is a change in Aditya's branch</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFDD0',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+

c2ed448a5f44a8e5fa7f743883a06406eafdc1bd
Adpro813 <adityad1108@gmail.com>
2024-08-12 19:48:47 -0700

    added a timer to move to the home screen from logo screen

--- myapp/App.js
+++ myapp/App.js
@@ -1,29 +1,55 @@
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return(
-      <View style = {styles.container}> 
-    <ImageLoader>
-    </ImageLoader>
-    <StatusBar style="auto" />
-  </View>
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+      <StatusBar style="auto" />
+    </NavigationContainer>
   );
 }
-const ImageLoader =  () => {
-  return(
+
+const LoadingScreen = ({ navigation }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace('Home Screen');
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [navigation]);
+
+  return (
     <View style={styles.container}>
       <Image
         source={require('./images/logo.webp')}
-        style={{width: 200, height: 200}} />
+        style={{ width: 200, height: 200 }}
+      />
     </View>
   );
-}
-const AppNavigator = () => {
-  
-}
+};
 
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+const AppNavigator = () => {
+  return (
+    <Stack.Navigator initialRouteName="LoadingScreen">
+      <Stack.Screen name="LoadingScreen" component={LoadingScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Home Screen" component={HomeScreen} options = {{headerShown:false}} />
+    </Stack.Navigator>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {

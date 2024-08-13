24654da803cd31487624078b5bb6feaec36dd275
Adpro813 <adityad1108@gmail.com>
2024-08-12 18:23:50 -0700

    refactord code to have seperate messages

--- myapp/App.js
+++ myapp/App.js
@@ -2,14 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
-  return (
+  return(
+      <View style = {styles.container}> 
+    <ImageLoader>
+    </ImageLoader>
+    <StatusBar style="auto" />
+  </View>
+  );
+}
+const ImageLoader =  () => {
+  return(
     <View style={styles.container}>
       <Image
         source={require('./images/logo.webp')}
         style={{width: 200, height: 200}} />
-      <StatusBar style="auto" />
     </View>
   );
+  
 }
 
 const styles = StyleSheet.create({

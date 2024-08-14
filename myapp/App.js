4f556d692deb73bc4f9d27a41f5e93f351f8708e
Adpro813 <adityad1108@gmail.com>
2024-08-14 15:46:46 -0700

    nothing changed

--- myapp/App.js
+++ myapp/App.js
@@ -72,7 +72,7 @@ function LogInScreen({ navigation }) {
           style={styles.guestText}
           onPress={() => navigation.navigate('HomeScreen')}
         >
-          Continue as Guest
+         Guest
         </Text>
 
         <Text

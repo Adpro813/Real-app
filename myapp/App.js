aef4162a14338a84ce116a710893c2de67ddd062
Adpro813 <adityad1108@gmail.com>
2024-10-27 13:49:52 -0700

    fixed bug in naming

--- myapp/App.js
+++ myapp/App.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import LoadingScreen from './components/LoadingScreen';
-import LogInScreen from './components/LogInScreen';
+import LoginScreen from './components/LoginScreen';
 import HomeScreen from './components/HomeScreen';
 import UsernamePasswordScreen from './components/UsernamePasswordScreen';
 
@@ -15,7 +15,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="LoadingScreen">
         <Stack.Screen name="LoadingScreen" component={LoadingScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Start Screen" component={LogInScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Start Screen" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name="UsernamePasswordScreen" component={UsernamePasswordScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
